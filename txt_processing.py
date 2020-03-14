fff4fdc3fad4f1335f3131aa6aa327a5e6a99478
Ivan <ivan.ilyukhin@phystech.edu>
2020-03-14 12:59:33 +0300

    Update txt_processing.py
    added some descriptions
    added function "sort_arrays"

--- txt_processing.py
+++ txt_processing.py
@@ -1,10 +1,21 @@
 #Processing of txt files, which were taken from CGNS
 
+"""
+get_data:
+input: file_name (str)
+output: var_names (array of str),
+        data (array of data arrays, float)
+        x_points (array of x coordinates)
+"""
+
 def get_data(file_name):
+
     f = open(file_name)
     names = f.readline()
     data = f.readlines()
     f.close()
+
+
     var_names = names.split()
     num_vars = len(var_names)
     clear_data = []
@@ -27,15 +38,44 @@ def get_data(file_name):
                     x_points.append(x)
     return var_names, clear_data, x_points
 
+def sort_arrays(y, f_y):
+    presort = []
+    for i in range(len(y)):
+        presort.append([y[i], f_y[i]])
+
+    presort.sort(key = lambda x: x[0])
+    y_sorted = []
+    var_sorted = []
+    for i in range (len(y)):
+        y_sorted.append(presort[i][0])
+        var_sorted.append(presort[i][1])
+    return y_sorted, var_sorted
+
+'''
+get_xcolumn: collects variable over y in choosen x slice
+input:  x (x slice)
+        var_name
+        var_names
+        data
+
+output: y_column (array of float)
+        var_column (array of float)
+'''
+
 def get_xcolumn(x, var_name, var_names, data):
+
     num_column = var_names.index(var_name)
+
     var_column = []
     y_column = []
+
     for line in data:
         if line[0] == x:
             y_column.append(line[1])
             var_column.append(line[num_column])
-    return y_column, var_column
+
+    y, var = sort_arrays(y_column, var_column)
+    return y, var
 
 def get_node_x(approximate_x, x_points):
     node_x = 0
